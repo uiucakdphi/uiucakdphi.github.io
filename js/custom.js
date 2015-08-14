2229b65e2ae62c807cacf89e1275445316b1239b
James W. Lee <leejamesws@gmail.com>
2015-08-14 03:44:39 -0700

    event page fixed

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+	new WOW().init();
+
+	jQuery(window).load(function() { 
+		jQuery("#preloader").delay(100).fadeOut("slow");
+		jQuery("#load").delay(100).fadeOut("slow");
+	});
+
+
+
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		
+		$('.page-scroll a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+})(jQuery);
